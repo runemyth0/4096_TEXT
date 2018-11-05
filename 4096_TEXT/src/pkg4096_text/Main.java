62aed61e7cf553c9afc66cb81bf1326148f8fac6
Lyle Costo <runemyth0@gmail.com>
2018-11-05 10:32:30 -0600

    11/05 work
    WASD-based movement started, rudimentary value movement begun

--- 4096_TEXT/src/pkg4096_text/Main.java
+++ 4096_TEXT/src/pkg4096_text/Main.java
@@ -1,11 +1,12 @@
 package pkg4096_text;
-import java.util.Random; 
+import java.util.Random;
+import java.util.Scanner;
 
 /*
 Project: 2048 Java Clone
 File Name: 4096_TEXT
 Author: James Costo
-Last Compiled: 10/31/2018 10:31 AM
+Last Compiled: 11/05/2018 9:34 AM
 Compiled in NetBeans 8.2
 */
 
@@ -16,8 +17,11 @@ public class Main
         Random rand = new Random();
         long[][] board = new long[4][4];
         boolean retry = true;
+        Scanner keyboard = new Scanner (System.in);
+        char movement;
+        int xMove = 0, yMove = 0;
         
-        System.out.println("Use the arrow keys to move the board.");
+        System.out.println("Use the WASD keys to move the board.");
         
         // insert game loop start here
         
@@ -30,6 +34,59 @@ public class Main
             {
                 retry = false;
                 board[y][x] = newValue();
+            }
+        }
+        showBoard(board);
+        
+        // Get movement key
+        movement = keyboard.nextLine().charAt(0);
+        
+        switch (movement)
+        {
+            case 'W':
+            case 'w':
+                yMove = -1;
+                break;
+            case 'S':
+            case 's':
+                yMove = 1;
+                break;
+            case 'A':
+            case 'a':
+                xMove = -1;
+                break;
+            case 'D':
+            case 'd':
+                xMove = 1;
+                break;
+        }
+        
+        // move stuff
+        System.out.println(yMove + " " + xMove);
+        boolean moving = true;
+        for (int iter = 0; iter <4; iter++)
+        {
+            if (yMove != 0)
+            {
+                if (yMove > 0)
+                {
+                    // start at bottom
+                    for (int y = 2; y >= 0; y--)
+                    {
+                        for (int x = 0; x < 4; x++)
+                        {
+                            if (board[y+yMove][x] == 0)
+                            {
+                                board[y+yMove][x] = board[y][x];
+                                board[y][x] = board[y-yMove][x];
+                            }
+                        }
+                    }
+                }
+                else if (yMove < 0)
+                {
+                    // start at top
+                }
             }
         }
         showBoard(board);
